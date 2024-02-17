233b1d71636f10c626ece0529826bb608d098b98
MAXBread <mvlmvl34@gmail.com>
2024-02-17 19:24:27 +0200

    picture

--- main/views.py
+++ main/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 
 def home(request):
+    if request.POST:
+        if 'btn-tablet' in request.POST:
+            print('btn-tab')
+        elif 'btn-newprint' in request.POST:
+            print('btn-new')
+        elif 'btn-delete' in request.POST:
+            print('btn-del')
     return render(request,'main/index.html')
