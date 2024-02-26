2a0a1ae4d845ca9def0e0f57cae788a71745aceb
MAXBread <mvlmvl34@gmail.com>
2024-02-26 20:11:20 +0200

    add chain directory

--- main/models.py
+++ main/models.py
@@ -3,10 +3,9 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=20, db_index=True, unique=True)
-    image = models.ImageField(upload_to='icons, recipes')
+    image = models.ImageField(upload_to='icons/items')
 
 
 class Recipe (models.Model):
     name = models.CharField(max_length=20, db_index=True, unique=True)
-    image = models.ImageField(upload_to='icons, recipes')
-
+    image = models.ImageField(upload_to='icons/items')
